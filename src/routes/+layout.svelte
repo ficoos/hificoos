649812acd52aed31b2092c73b55636b43a95c95b
Saggi Mizrahi <saggi@mizrahi.cc>
2026-08-06 20:50:36 +0300

    Add daisyui

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,21 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { resolve } from '$app/paths';
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<div class="navbar bg-base-100 shadow-sm">
+  <div class="flex-none">
+  <!-- Start aligned content -->
+  </div>
+  <div class="flex-1">
+    <a class="btn btn-ghost text-xl" href={resolve("/")}>HiFiCoos</a>
+  </div>
+  <div class="flex-none">
+  <!-- End aligned content -->
+  </div>
+</div>
 {@render children()}
